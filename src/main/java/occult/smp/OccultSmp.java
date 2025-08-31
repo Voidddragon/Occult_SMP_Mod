471e79cd0029fef5eccc3b5bc522a884f71df715
Voidddragon <somonevoided@outlook.com>
2025-08-31 18:22:19 -0400

    w

---
+++ src/main/java/occult/smp/OccultSmp.java
@@ -0,0 +1,16 @@
+package occult.smp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OccultSmp implements ModInitializer {
+	public static final String MOD_ID = "occult-smp";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/occult/smp/OccultSmpClient.java
@@ -0,0 +1,10 @@
+package occult.smp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OccultSmpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
